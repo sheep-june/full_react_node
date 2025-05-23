8422382155b6b26edb45ff5a1ded5841647f0832
sheepjune-jp <jun8547@gmail.com>
2025-05-23 18:08:42 +0900

    오류수정

--- frontend/src/pages/LandingPage/Sections/CheckBox.jsx
+++ frontend/src/pages/LandingPage/Sections/CheckBox.jsx
@@ -1,40 +1,4 @@
-// import React from "react";
 
-// const CheckBox = ({ items, checkedItems, onFilters }) => {
-//     const handleToggle = (itemId) => {
-//         const currentIndex = checkedItems.indexOf(itemId);
-//         const newChecked = [...checkedItems];
-
-//         if (currentIndex === -1) {
-//             newChecked.push(itemId);
-//         } else {
-//             newChecked.splice(currentIndex, 1);
-//         }
-
-//         onFilters(newChecked);
-//     };
-
-//     return (
-//         <div className="p-2 mb-3 bg-gray-100 rounded-md">
-//             {items?.map((item) => (
-//                 <div key={item._id}>
-//                     <input
-//                         type="checkbox"
-//                         onChange={() => handleToggle(item._id)}
-//                         checked={checkedItems.includes(item._id)}
-//                     />
-//                     <label className="ml-2">{item.name}</label>
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// };
-
-// export default CheckBox;
-
-
-
-// ✅ 수정된 CheckBox 컴포넌트
 import React from "react";
 
 const CheckBox = ({ items, checkedItems, onFilters }) => {
